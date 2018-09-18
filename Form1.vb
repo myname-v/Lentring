0ffa416b273ae23a7553b02bb37f00993511a3e2
V <vlogs@naver.com>
2018-09-18 11:33:17 +0900

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+	Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+		Me.Text = "Lentring - 문자열 계산기 (" & TextBox1.TextLength & "글자)"
+	End Sub
+
+	Private Sub TextBox1_DoubleClick(sender As Object, e As EventArgs) Handles TextBox1.DoubleClick
+		TextBox1.Text = ""
+	End Sub
+End Class
